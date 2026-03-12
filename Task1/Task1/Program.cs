39565f61a5ef77826cb8b552e6d9d120b3d26907
Huseyn483 <huseynli2006huseyn@gmail.com>
2026-03-12 12:12:57 +0400

    task1

---
+++ Task1/Task1/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace Task1;
+
+/*public class HelloWorld
+{
+	public static void Main(string[] args)
+	{
+		string soz = Console.ReadLine();
+		int h = 1;
+		int s = 1;
+		int f = 1;
+		for (int i = 0; i < soz.Length; i++)
+		{
+			if (char.IsUpper(soz[i]))
+			{
+				h = 0;
+			}
+			else if (char.IsLower(soz[i]))
+			{
+				s = 0;
+			}
+			else if (char.IsDigit(soz[i]))
+			{
+				f = 0;
+			}
+		}
+
+		if (f == 0 && s == 0 && h == 0)
+		{
+			Console.WriteLine("Sertleri odeyir");
+		}
+		else
+		{
+			Console.WriteLine("Sertleri odemir");
+		}
+	}
+}
+*/
+
+/*
+public class HelloWorld
+{
+	public static void Main(string[] args)
+	{
+		string cumle = "Salam sagol";
+		string[] arr = cumle.Split(" ");
+		if (arr.Length == 2 && char.IsUpper(arr[0][0]) & char.IsUpper(arr[1][0]))
+		{
+			Console.WriteLine("Sert odenir");
+		}
+		else
+		{
+			Console.WriteLine("Sert odenmir");
+		}
+
+	}
+}
+*/
+
+
